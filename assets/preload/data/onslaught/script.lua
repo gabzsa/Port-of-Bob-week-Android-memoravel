d6742136fc4c8c1f5a8508051275699a67121d91
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-09-07 13:57:37 -0300

    Add files via upload

---
+++ assets/preload/data/onslaught/script.lua
@@ -0,0 +1,61 @@
+function onBeatHit()
+	if curBeat == 0 or curBeat == 1 or curBeat == 2 or curBeat == 3 or curBeat == 4 or curBeat == 5 or curBeat == 6 or curBeat == 7 or curBeat == 8 or curBeat == 9 or curBeat == 10 or curBeat == 11 or curBeat == 12 or curBeat == 13 or curBeat == 14 or curBeat == 15 or curBeat == 16 or curBeat == 17 or curBeat == 18 or curBeat == 19 or curBeat == 20 or curBeat == 21 or curBeat == 22 or curBeat == 23 or curBeat == 24 or curBeat == 25 or curBeat == 26 or curBeat == 27 or curBeat == 28 or curBeat == 29 or curBeat == 30 or curBeat == 31 or curBeat == 32 or curBeat == 33 or curBeat == 34 or curBeat == 35 or curBeat == 36 or curBeat == 37 or curBeat == 38 or curBeat == 39 or curBeat == 40 or curBeat == 41 or curBeat == 42 or curBeat == 43 or curBeat == 44 or curBeat == 45 or curBeat == 46 or curBeat == 47 or curBeat == 48 or curBeat == 49 or curBeat == 50 or curBeat == 51 or curBeat == 52 or curBeat == 53 or curBeat == 54 or curBeat == 55 or curBeat == 56 or curBeat == 57 or curBeat == 58 or curBeat == 59 or curBeat == 60 or curBeat == 61 or curBeat == 62 or curBeat == 63 or curBeat == 64 then
+		triggerEvent('Add Camera Zoom', '', '')
+	end
+
+	if curBeat == 96 or curBeat == 97 or curBeat == 98 or curBeat == 99 or curBeat == 100 or curBeat == 101 or curBeat == 102 or curBeat == 103 or curBeat == 104 or curBeat == 105 or curBeat == 106 or curBeat == 107 or curBeat == 108 or curBeat == 109 or curBeat == 110 or curBeat == 111 or curBeat == 112 or curBeat == 113 or curBeat == 114 or curBeat == 115 or curBeat == 116 or curBeat == 117 or curBeat == 118 or curBeat == 119 or curBeat == 120 or curBeat == 121 or curBeat == 122 or curBeat == 123 or curBeat == 124 or curBeat == 125 or curBeat == 126 or curBeat == 127 or curBeat == 128 or curBeat == 129 or curBeat == 130 or curBeat == 131 or curBeat == 132 or curBeat == 133 or curBeat == 134 or curBeat == 135 or curBeat == 136 or curBeat == 137 or curBeat == 138 or curBeat == 139 or curBeat == 140 or curBeat == 141 or curBeat == 142 or curBeat == 143 or curBeat == 144 or curBeat == 145 or curBeat == 146 or curBeat == 147 or curBeat == 148 or curBeat == 149 or curBeat == 150 or curBeat == 151 or curBeat == 152 or curBeat == 153 or curBeat == 154 or curBeat == 155 or curBeat == 156 or curBeat == 157 or curBeat == 158 or curBeat == 159 or curBeat == 160 or curBeat == 161 or curBeat == 162 or curBeat == 163 or curBeat == 164 or curBeat == 165 or curBeat == 166 or curBeat == 167 or curBeat == 168 or curBeat == 169 or curBeat == 170 or curBeat == 171 or curBeat == 172 or curBeat == 173 or curBeat == 174 or curBeat == 175 or curBeat == 176 or curBeat == 177 or curBeat == 178 or curBeat == 179 or curBeat == 180 or curBeat == 181 or curBeat == 182 or curBeat == 183 or curBeat == 184 or curBeat == 185 or curBeat == 186 or curBeat == 187 or curBeat == 188 or curBeat == 189 or curBeat == 190 or curBeat == 191 or curBeat == 192 or curBeat == 193 or curBeat == 194 or curBeat == 195 or curBeat == 196 or curBeat == 197 or curBeat == 198 or curBeat == 199 or curBeat == 200 or curBeat == 201 or curBeat == 202 or curBeat == 203 or curBeat == 204 or curBeat == 205 or curBeat == 206 or curBeat == 207 or curBeat == 208 or curBeat == 209 or curBeat == 210 or curBeat == 211 or curBeat == 212 or curBeat == 213 or curBeat == 214 or curBeat == 215 or curBeat == 216 or curBeat == 217 or curBeat == 218 or curBeat == 219 or curBeat == 220 or curBeat == 221 or curBeat == 222 or curBeat == 223 or curBeat == 224 then
+		triggerEvent('Add Camera Zoom', '', '')
+	end
+
+	if curBeat == 240 or curBeat == 241 or curBeat == 242 or curBeat == 243 or curBeat == 244 or curBeat == 245 or curBeat == 246 or curBeat == 247 or curBeat == 248 or curBeat == 249 or curBeat == 250 or curBeat == 251 or curBeat == 252 or curBeat == 253 or curBeat == 254 or curBeat == 255 or curBeat == 256 or curBeat == 257 or curBeat == 258 or curBeat == 259 or curBeat == 260 or curBeat == 261 or curBeat == 262 or curBeat == 263 or curBeat == 264 or curBeat == 265 or curBeat == 266 or curBeat == 267 or curBeat == 268 or curBeat == 269 or curBeat == 270 or curBeat == 271 or curBeat == 272 or curBeat == 273 or curBeat == 274 or curBeat == 275 or curBeat == 276 or curBeat == 277 or curBeat == 278 or curBeat == 279 or curBeat == 280 or curBeat == 281 or curBeat == 282 or curBeat == 283 or curBeat == 284 or curBeat == 285 or curBeat == 286 or curBeat == 287 or curBeat == 288 or curBeat == 289 or curBeat == 290 or curBeat == 291 or curBeat == 292 or curBeat == 293 or curBeat == 294 or curBeat == 295 or curBeat == 296 or curBeat == 297 or curBeat == 298 or curBeat == 299 or curBeat == 300 or curBeat == 301 or curBeat == 302 or curBeat == 303 or curBeat == 304 or curBeat == 305 or curBeat == 306 or curBeat == 307 or curBeat == 308 or curBeat == 309 or curBeat == 310 or curBeat == 311 or curBeat == 312 or curBeat == 313 or curBeat == 314 or curBeat == 315 or curBeat == 316 or curBeat == 317 or curBeat == 318 or curBeat == 319 or curBeat == 320 or curBeat == 321 or curBeat == 322 or curBeat == 323 or curBeat == 324 or curBeat == 325 or curBeat == 326 or curBeat == 327 or curBeat == 328 or curBeat == 329 or curBeat == 330 or curBeat == 331 or curBeat == 332 or curBeat == 333 or curBeat == 334 or curBeat == 335 or curBeat == 336 or curBeat == 337 or curBeat == 338 or curBeat == 339 or curBeat == 340 or curBeat == 341 or curBeat == 342 or curBeat == 343 or curBeat == 344 or curBeat == 345 or curBeat == 346 or curBeat == 347 or curBeat == 348 or curBeat == 349 or curBeat == 350 or curBeat == 351 or curBeat == 352 then
+		triggerEvent('Add Camera Zoom', '', '')
+	end
+
+	if curBeat == 96 then
+		playSound('Meow')
+		for i = 0, getProperty('playerStrums.length')-1 do
+			setPropertyFromGroup('playerStrums', i, 'texture', 'invisiblenote')
+		end
+		for i = 0, getProperty('opponentStrums.length')-1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', 'invisiblenote')
+		end
+	end
+	
+	if curBeat == 128 then
+		noteTweenAngle("spin0", 4, 8310, 22.4, 'linear')
+		noteTweenAngle("spin1", 5, 7512, 22.4, 'linear')
+		noteTweenAngle("spin2", 6, 4480, 22.4, 'linear')
+		noteTweenAngle("spin3", 7, 6704, 22.4, 'linear')
+		playSound('woeM')
+		for i = 0, getProperty('playerStrums.length')-1 do
+			setPropertyFromGroup('playerStrums', i, 'texture', 'NOTE_assets')
+		end
+		for i = 0, getProperty('opponentStrums.length')-1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', 'NOTE_assets')
+		end
+	end
+
+	if curBeat == 240 then
+		noteTweenAngle("still0", 4, 0, 0.001, 'linear')
+		noteTweenAngle("still1", 5, 0, 0.001, 'linear')
+		noteTweenAngle("still2", 6, 0, 0.001, 'linear')
+		noteTweenAngle("still3", 7, 0, 0.001, 'linear')
+		playSound('Meow')
+		for i = 0, getProperty('playerStrums.length')-1 do
+			setPropertyFromGroup('playerStrums', i, 'texture', 'invisiblenote')
+		end
+		for i = 0, getProperty('opponentStrums.length')-1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', 'invisiblenote')
+		end
+	end
+	
+	if curBeat == 352 then
+		playSound('woeM')
+		for i = 0, getProperty('playerStrums.length')-1 do
+			setPropertyFromGroup('playerStrums', i, 'texture', 'NOTE_assets')
+		end
+		for i = 0, getProperty('opponentStrums.length')-1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', 'NOTE_assets')
+		end
+	end
+end
