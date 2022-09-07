2c760bd1c9d94dd33404fd3253634b2ac7759b07
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-09-07 13:58:49 -0300

    Add files via upload

---
+++ assets/preload/data/run/script.lua
@@ -0,0 +1,447 @@
+function onStepHit()
+	if curBeat < 220 then
+		if curStep % 2 == 0 then
+			triggerEvent('Screen Shake', '0.2, 0.005', '0.2, 0.02')
+		end
+	end
+
+	if curStep == 16 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 24 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 44 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 50 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 52 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 58 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 74 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 80 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 106 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 112 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 124 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 130 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 158 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 164 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 184 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 190 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 196 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 202 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 204 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 210 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 246 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 252 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 320 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 326 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 354 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 360 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 362 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 368 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 392 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 398 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 416 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 422 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 440 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 446 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 448 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 454 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 468 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 474 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 484 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 490 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 520 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 526 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 548 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 554 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 574 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 580 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 582 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 588 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 598 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 604 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 620 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 626 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 636 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 642 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 664 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 670 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 672 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 678 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 684 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 690 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 718 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 724 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 730 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 736 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 746 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 752 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 766 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 772 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 782 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 788 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 794 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 800 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 808 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 814 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 822 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 828 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 832 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 838 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+
+	if curStep == 850 then
+		setProperty('camHUD.visible', false)
+		setProperty('bobmadshake.visible', true)
+		playSound('bobscreen')
+	end
+	
+	if curStep == 856 then
+		setProperty('camHUD.visible', true)
+		setProperty('bobmadshake.visible', false)
+	end
+end
