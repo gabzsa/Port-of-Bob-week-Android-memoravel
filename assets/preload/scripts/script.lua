12f2f40a592d954a5985567a7b8dad35316324bc
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-09-07 14:13:41 -0300

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-spiked');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx-BOB');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver-BOB');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd-BOB');
+end
